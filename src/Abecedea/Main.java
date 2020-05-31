edaa131bb2c97b9f243a182294957cbc074fa737
FilipKrumpholc <filish@email.cz>
2020-05-31 20:33:34 +0200

    Abeceda

---
+++ src/Abecedea/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Abeceda a = new Abeceda();
+    Abeceda b = new Abeceda();
+    Abeceda c = new Abeceda();
+    Abeceda d = new Abeceda();
+    Abeceda e = new Abeceda();
+    Abeceda f = new Abeceda();
+	
+	
+	}
+
+}
