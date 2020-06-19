7a2e3ca86e4e56bcaebd4e16f74336f26782311c
Tepsta <65709699+Tepsta@users.noreply.github.com>
2020-06-19 09:17:14 +0200

    Přidal jsem jednotky, metoda Main a double tuny.

--- src/src/Prevodnik.java
+++ src/src/Prevodnik.java
@@ -1,32 +1,50 @@
 
+
 public class Prevodnik {
 	double kilogramy;
 	double gramy;
+	double tuny;
 
-	public double Prevodnikzkilogramunagramy(double kilogramy) {
-
-		return kilogramy/1000;
-
+	public String PrevodnikKilogramuNaGramy(double kilogramy) {
+		return kilogramy + "kg = " + kilogramy * 1000 + "g";
 	}
-	public double Prevodnikzgramunakilogramy(double gramy) {
 
-		return gramy*1000;
-	
+	public String PrevodnikGramuNaKilogramy(double gramy) {
+		return gramy + "g = " + gramy / 1000 + "kg";
 	}
-	
-	public double getKilogramy() {
-		return kilogramy;
+
+	public String PrevodnikTunuNaGramy(double tuny) {
+		return tuny + "t = " + tuny * 1000000 + "g";
+	}
+
+	public String PrevodnikGramuNaTuny(double gramy) {
+		return gramy + "g = " + gramy / 1000000 + "t";
+	}
+
+	public String PrevodnikTunuNaKilogramy(double tuny) {
+		return tuny + "t = " + tuny * 1000 + "kg";
+	}
+
+	public String PrevodnikKilogramuNaTuny(double kilogramy) {
+		return kilogramy + "kg = " + kilogramy / 1000 + "t";
 	}
 
 	public void setKilogramy(double kilogramy) {
 		this.kilogramy = kilogramy;
 	}
 
-	public double getGramy() {
-		return gramy;
-	}
-
 	public void setGramy(double gramy) {
 		this.gramy = gramy;
+	}
+
+	public void setTuny(double tuny) {
+		this.tuny = tuny;
+	}
+
+	public static void main(String[] args) {
+		Prevodnik p = new Prevodnik();
+		int a = 50;
+		p.setKilogramy(a);
+		System.out.println(p.PrevodnikKilogramuNaGramy(a));
 	}
 }
